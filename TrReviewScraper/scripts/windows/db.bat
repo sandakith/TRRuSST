2a18bc4eb5d50eaf8f905754542f8d2cfd7a46fc
sandakith <sandakith@gmail.com>
2014-11-04 12:09:29 -0500

    adding new config files for the scraper

---
+++ TrReviewScraper/scripts/windows/db.bat
@@ -0,0 +1,3 @@
+cd %SCRAPER_ROOT%\droidReviewDB
+start java -jar %DERBY_INSTALL%\lib\derbyrun.jar server start
+cd %SCRAPER_ROOT%\scripts\windows
---
+++ TrReviewScraper/scripts/windows/tables.sql
@@ -0,0 +1,5 @@
+connect 'jdbc:derby://localhost:1527/droidReviewDB;create=true';
+
+connect 'jdbc:derby://localhost:1527/droidReviewDB';
+
+run 'sql\Market_DB_Setup.sql';
